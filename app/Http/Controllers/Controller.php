9ddef67035a061b3d49d56ac5496837fcea6ac64
nasa <nasaruddinhr@gmail.com>
2026-04-12 21:42:05 +0800

    ubah title

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-import Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
-    use AuthorizesRequests, ValidatesRequests;
+
 }
