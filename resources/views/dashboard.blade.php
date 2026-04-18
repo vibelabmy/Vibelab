8f8749cf017d5fe8aef5c56c0f01c421317afe1c
nasa <nasaruddinhr@gmail.com>
2026-04-19 02:42:59 +0800

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
-                <h1>hellow world</h1>
+                <h1>haloo</h1>
             </div>
         </div>
     </div>
